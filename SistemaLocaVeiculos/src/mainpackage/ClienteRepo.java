3c50553c3a347cd842889f39e7568f6fbe0466b1
CaixoDarksixo <jpnossol@gmail.com>
2025-06-22 16:15:48 -0300

    Update ClienteRepo.java
    Agora ele também salva quando exportado em .jar

--- SistemaLocaVeiculos/src/mainpackage/ClienteRepo.java
+++ SistemaLocaVeiculos/src/mainpackage/ClienteRepo.java
@@ -6,28 +6,29 @@ import java.util.List;
 
 public class ClienteRepo {
 
-    private static final String ARQUIVO = "clientes.dat";
+    private static final String ARQUIVO = "clientes.dat"; 
 
     @SuppressWarnings("unchecked")
     public static List<Cliente> load() {
-        File f = new File(ARQUIVO);
-        if (!f.exists()) {           
+        FileInputStream f;
+        try {
+            f = new FileInputStream(ARQUIVO);
+        } catch (FileNotFoundException e) {
             return new ArrayList<>();
         }
 
         try (ObjectInputStream ois =
-                new ObjectInputStream(new FileInputStream(f))) {
+                     new ObjectInputStream(f)) {
             return (ArrayList<Cliente>) ois.readObject();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
-
             return new ArrayList<>();
         }
     }
 
     public static void save(List<Cliente> lista) {
         try (ObjectOutputStream oos =
-                new ObjectOutputStream(new FileOutputStream(ARQUIVO))) {
+                     new ObjectOutputStream(new FileOutputStream(ARQUIVO))) {
             oos.writeObject(lista);
         } catch (IOException e) {
             e.printStackTrace();
